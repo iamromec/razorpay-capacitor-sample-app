2a4b64f003e15c75adad22844017763e8015287e
Meghraj Suthar <iamromec@gmail.com>
2021-03-22 17:56:36 +0530

    added razorpay checkout button

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import  'capacitor-razorpay';
+import { Plugins } from '@capacitor/core';
+const { Checkout } = Plugins;
 
 @Component({
   selector: 'app-explore-container',
@@ -11,5 +14,31 @@ export class ExploreContainerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {}
+
+  private options = {
+    description: 'Description',
+    currency: 'INR', // your 3 letter currency code
+    key: 'rzp_test_1DP5mmOlF5G5ag', // your Key Id from Razorpay dashboard
+    amount: '5000', // Payment amount in smallest denomiation e.g. cents for USD
+    name: 'Sample App',
+    prefill: {
+      email: 'mail@example.com',
+      contact: '9999999999',
+      name: 'Raj Suthar'
+    },
+    theme: {
+      color: '#000000'
+    }
+  }
+
+  async pay() {
+    try {
+      let data = (await Checkout.open(this.options));
+      console.log(data.response);
+      alert(data.response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
 }
